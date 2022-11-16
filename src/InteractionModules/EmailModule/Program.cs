08fdb997faaa8ac72652cebf49f4779653eb0710
novg <novg.novg@gmail.com>
2022-11-16 13:28:28 +0700

    Init the project

---
+++ src/InteractionModules/EmailModule/Program.cs
@@ -0,0 +1,10 @@
+using EmailModule;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ tests/Intrastructure.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Intrastructure.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
