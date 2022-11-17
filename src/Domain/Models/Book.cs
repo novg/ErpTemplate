f022f87c81b99ca2ef5205b6ae074904c06984c2
novg <novg.novg@gmail.com>
2022-11-17 15:34:55 +0700

    Add not mapped Count property

--- src/Domain/Models/Book.cs
+++ src/Domain/Models/Book.cs
@@ -9,6 +9,7 @@ public class Book
     public string? Description { get; set; }
 
     public decimal Price { get; set; }
+    public int Count { get; set; }
 
     public ICollection<Order> Orders { get; set; } = new List<Order>();
     public ICollection<BookOrder> OrdersLink { get; set; } = new List<BookOrder>();
