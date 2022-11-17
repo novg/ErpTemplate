c377ef9338591ffa735dd3852d40439e94538927
novg <novg.novg@gmail.com>
2022-11-17 15:37:08 +0700

    Add order repository tests

---
+++ tests/Intrastructure.UnitTests/Repositories/OrderRepositoryTests.cs
@@ -0,0 +1,60 @@
+using Xunit;
+using System;
+using System.Threading.Tasks;
+using Infrastructure.Persistence;
+using Infrastructure.Repositories;
+using Application.Interfaces.Repositories;
+using Domain.Models;
+using Domain.Enums;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Intrastructure.UnitTests;
+
+public class OrderRepositoryTests : IDisposable
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IOrderRepository _repository;
+
+    public OrderRepositoryTests()
+    {
+        _context = Helpers.CreateDbContext();
+        _repository = new OrderRepository(_context);
+        Assert.NotNull(_repository);
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+
+    [Fact]
+    public async Task CreateOrder_Success()
+    {
+        // Arrange
+        int bookId = 1;
+        Book? book = await _context.Books.FindAsync(bookId);
+        book!.Count = 3;
+
+        Order order = new()
+        {
+            Number = "20221101",
+            ClientType = ClientType.Api,
+        };
+
+        order.Books.Add(book);
+
+        // Act
+        Order newOrder = await _repository.CreateOrder(order);
+
+        // Assert
+        Assert.NotNull(newOrder);
+        Assert.Equal(order.Number, newOrder.Number);
+        Assert.Equal(order.ClientType, newOrder.ClientType);
+
+        Assert.NotEmpty(newOrder.Books);
+        Assert.Equal(order.Books.Count, newOrder.Books.Count);
+        Assert.Equal(book.Id, newOrder.Books.First().Id);
+        Assert.Equal(book.Name, newOrder.Books.First().Name);
+    }
+}
