208133fbeddae19b1207790efaa950f53ed935aa
novg <novg.novg@gmail.com>
2022-11-20 17:46:10 +0700

    Fix xml model naming

--- src/Application/Models/BookInput.cs
+++ src/Application/Models/BookInput.cs
@@ -1,17 +1,21 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Xml.Serialization;
 
 namespace Application.Models;
 
-[XmlType(TypeName = "Book")]
+[XmlType(TypeName = "book")]
 public class BookInput
 {
     [Required]
+    [Range(1, Int32.MaxValue)]
+    [DefaultValue(1)]
     [XmlElement("id")]
     public int Id { get; set; }
 
     [Required]
     [Range(1, 1000)]
+    [DefaultValue(1)]
     [XmlElement("count")]
     public int Count { get; set; }
 }
